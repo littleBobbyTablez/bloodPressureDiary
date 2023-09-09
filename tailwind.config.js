9fe5d87c1e9361b8beb8219be597210806ced2d8
littleBobbyTabelz <jonas.st@mailbox.org>
2023-09-09 22:44:49 +0200

    add fade in animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ["./statics/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+        animation: {
+            fadeInFast: 'fadeIn 500ms ease-in-out' ,
+        },
+        keyframes: {
+            fadeIn: {
+                '0%': { opacity: 0 },
+                '100%': { opacity: 100},
+            },
+        }
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addVariant }) {
+      addVariant('htmx-settling', ['&.htmx-settling', '.htmx-settling &'])
+      addVariant('htmx-request',  ['&.htmx-request',  '.htmx-request &'])
+      addVariant('htmx-swapping', ['&.htmx-swapping', '.htmx-swapping &'])
+      addVariant('htmx-added',    ['&.htmx-added',    '.htmx-added &'])
+    }),
+  ],
 }
-
