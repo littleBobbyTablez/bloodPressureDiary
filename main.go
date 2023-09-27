61f1244bef8a7a7721cb2aa8695c79cf0062e82a
littleBobbyTabelz <jonas.st@mailbox.org>
2023-09-27 09:49:05 +0200

    streamline deletion process

--- main.go
+++ main.go
@@ -1,280 +1,280 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"math"
-	"net/http"
-	"strconv"
+    "fmt"
+    "log"
+    "math"
+    "net/http"
+    "strconv"
 
-	"time"
+    "time"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/mattn/go-sqlite3"
+    "github.com/gin-gonic/gin"
+    _ "github.com/mattn/go-sqlite3"
 )
 
 type entry struct {
-	Id    int
-	Sys   int64
-	Dys   int64
-	Puls  int64
-	Sport bool
-	T     time.Time
+    Id    int
+    Sys   int64
+    Dys   int64
+    Puls  int64
+    Sport bool
+    T     time.Time
 }
 
 type entryFormat struct {
-	Id    int
-	Sys   int64
-	Dys   int64
-	Puls  int64
-	Sport bool
-	T     string
+    Id    int
+    Sys   int64
+    Dys   int64
+    Puls  int64
+    Sport bool
+    T     string
 }
 
 type values struct {
-	Sys  int64
-	Dys  int64
-	Puls int64
-	T    string
+    Sys  int64
+    Dys  int64
+    Puls int64
+    T    string
 }
 
 type pageType struct {
-	Page     int
-	Pagesize int
-	Active   bool
-	OrderBy  string
-	Order    string
+    Page     int
+    Pagesize int
+    Active   bool
+    OrderBy  string
+    Order    string
 }
 
 func main() {
-	db := connectDb()
+    db := connectDb()
 
-	r := gin.Default()
+    r := gin.Default()
 
-	r.LoadHTMLGlob("statics/*.html")
+    r.LoadHTMLGlob("statics/*.html")
 
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{})
-	})
+    r.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.html", gin.H{})
+    })
 
-	r.GET("/historyChart", func(c *gin.Context) {
-		from := c.Query("from")
-		to := c.Query("to")
-		e, err := readEntries(db, SqlParams{
-			offset:  0,
-			orderBy: "t",
-			order:   "ASC",
-			from:    from,
-			to:      to,
-			limit:   math.MaxInt,
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
+    r.GET("/historyChart", func(c *gin.Context) {
+        from := c.Query("from")
+        to := c.Query("to")
+        e, err := readEntries(db, SqlParams{
+            offset:  0,
+            orderBy: "t",
+            order:   "ASC",
+            from:    from,
+            to:      to,
+            limit:   math.MaxInt,
+        })
+        if err != nil {
+            log.Fatal(err)
+        }
 
-		lab, sys, dys, puls := generateData(e)
+        lab, sys, dys, puls := generateData(e)
 
-		c.HTML(http.StatusOK, "chart.html", gin.H{"sys": sys, "dys": dys, "puls": puls, "lab": lab, "from": from, "to": to})
-	})
+        c.HTML(http.StatusOK, "chart.html", gin.H{"sys": sys, "dys": dys, "puls": puls, "lab": lab, "from": from, "to": to})
+    })
 
-	r.GET("/edit", func(c *gin.Context) {
-		defaultPageSize := 25
-		e, err := readEntries(db, SqlParams{
-			offset:  0,
-			orderBy: "t",
-			order:   "ASC",
-			from:    "",
-			to:      "",
-			limit:   defaultPageSize,
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
+    r.GET("/edit", func(c *gin.Context) {
+        defaultPageSize := 25
+        e, err := readEntries(db, SqlParams{
+            offset:  0,
+            orderBy: "t",
+            order:   "ASC",
+            from:    "",
+            to:      "",
+            limit:   defaultPageSize,
+        })
+        if err != nil {
+            log.Fatal(err)
+        }
 
-		var data []entryFormat
-		for _, en := range e {
+        var data []entryFormat
+        for _, en := range e {
 
-			data = append(data, entryFormat{en.Id, en.Sys, en.Dys, en.Puls, en.Sport, en.T.Format(time.DateOnly)})
-		}
+            data = append(data, entryFormat{en.Id, en.Sys, en.Dys, en.Puls, en.Sport, en.T.Format(time.DateOnly)})
+        }
 
-		count := getEntryCount(db, "", "")
-		pages := ((count - 1) / defaultPageSize) + 1
+        count := getEntryCount(db, "", "")
+        pages := ((count - 1) / defaultPageSize) + 1
 
-		a := make([]pageType, pages)
+        a := make([]pageType, pages)
 
-		for i := 1; i <= pages; i++ {
-			active := i == 1
-			a[i-1] = pageType{i, 10, active, "t", "ASC"}
-		}
+        for i := 1; i <= pages; i++ {
+            active := i == 1
+            a[i-1] = pageType{i, 10, active, "t", "ASC"}
+        }
 
-		c.HTML(http.StatusOK, "edit.html", gin.H{"data": data, "pages": a, "active": 1, "orderBy": "t", "order": "ASC", "symbol": "9650", "pagesize": defaultPageSize})
-	})
+        c.HTML(http.StatusOK, "edit.html", gin.H{"data": data, "pages": a, "active": 1, "orderBy": "t", "order": "ASC", "symbol": "9650", "pagesize": defaultPageSize})
+    })
 
-	r.GET("/table/:Page", func(c *gin.Context) {
-		param := c.Param("Page")
-		page, err := strconv.Atoi(param)
-		if err != nil {
-			log.Fatal(err)
-		}
+    r.GET("/table/:Page", func(c *gin.Context) {
+        param := c.Param("Page")
+        page, err := strconv.Atoi(param)
+        if err != nil {
+            log.Fatal(err)
+        }
 
-		sizeparam := c.Query("pagesize")
-		pagesize, err := strconv.Atoi(sizeparam)
-		if err != nil {
-			pagesize = 10
-		}
+        sizeparam := c.Query("pagesize")
+        pagesize, err := strconv.Atoi(sizeparam)
+        if err != nil {
+            pagesize = 10
+        }
 
-		orderBy := c.Query("orderBy")
-		order := c.Query("order")
-		from := c.Query("from")
-		to := c.Query("to")
+        orderBy := c.Query("orderBy")
+        order := c.Query("order")
+        from := c.Query("from")
+        to := c.Query("to")
 
-		count := getEntryCount(db, from, to)
-		pages := ((count - 1) / pagesize) + 1
-		if page > pages {
-			page = pages
-		}
-		noswitch, parseErr := strconv.ParseBool(c.Query("noswitch"))
-		if parseErr != nil {
-			noswitch = false
-		}
-		symbol := "9650"
+        count := getEntryCount(db, from, to)
+        pages := ((count - 1) / pagesize) + 1
+        if page > pages {
+            page = pages
+        }
+        noswitch, parseErr := strconv.ParseBool(c.Query("noswitch"))
+        if parseErr != nil {
+            noswitch = false
+        }
+        symbol := "9650"
 
-		if !noswitch {
-			if order == "ASC" {
-				order = "DESC"
-			} else {
-				order = "ASC"
-			}
-		}
+        if !noswitch {
+            if order == "ASC" {
+                order = "DESC"
+            } else {
+                order = "ASC"
+            }
+        }
 
-		if order == "DESC" {
-			symbol = "9660"
-		}
+        if order == "DESC" {
+            symbol = "9660"
+        }
 
-		offset := (page - 1) * pagesize
-		e, err2 := readEntries(db, SqlParams{
-			offset:  offset,
-			orderBy: orderBy,
-			order:   order,
-			from:    from,
-			to:      to,
-			limit:   pagesize,
-		})
-		if err2 != nil {
-			log.Fatal(err2)
-		}
+        offset := (page - 1) * pagesize
+        e, err2 := readEntries(db, SqlParams{
+            offset:  offset,
+            orderBy: orderBy,
+            order:   order,
+            from:    from,
+            to:      to,
+            limit:   pagesize,
+        })
+        if err2 != nil {
+            log.Fatal(err2)
+        }
 
-		var data []entryFormat
-		for _, en := range e {
+        var data []entryFormat
+        for _, en := range e {
 
-			data = append(data, entryFormat{en.Id, en.Sys, en.Dys, en.Puls, en.Sport, en.T.Format(time.DateOnly)})
-		}
+            data = append(data, entryFormat{en.Id, en.Sys, en.Dys, en.Puls, en.Sport, en.T.Format(time.DateOnly)})
+        }
 
-		a := make([]pageType, pages)
-		for i := 1; i <= pages; i++ {
-			active := i == page
-			a[i-1] = pageType{i, pagesize, active, orderBy, order}
-		}
+        a := make([]pageType, pages)
+        for i := 1; i <= pages; i++ {
+            active := i == page
+            a[i-1] = pageType{i, pagesize, active, orderBy, order}
+        }
 
-		c.HTML(http.StatusOK, "edit.html", gin.H{"data": data, "pages": a, "active": page, "orderBy": orderBy, "order": order, "symbol": symbol, "from": from, "to": to, "pagesize": pagesize})
-	})
+        c.HTML(http.StatusOK, "edit.html", gin.H{"data": data, "pages": a, "active": page, "orderBy": orderBy, "order": order, "symbol": symbol, "from": from, "to": to, "pagesize": pagesize})
+    })
 
-	r.GET("/entry/:Id", func(c *gin.Context) {
-		param := c.Param("Id")
-		id, err := strconv.ParseInt(param, 10, 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		var e entry
-		e, err = readEntry(db, int(id))
+    r.GET("/entry/:Id", func(c *gin.Context) {
+        param := c.Param("Id")
+        id, err := strconv.ParseInt(param, 10, 64)
+        if err != nil {
+            log.Fatal(err)
+        }
+        var e entry
+        e, err = readEntry(db, int(id))
 
-		c.HTML(http.StatusOK, "entry.html", gin.H{"entry": entryFormat{e.Id, e.Sys, e.Dys, e.Puls, e.Sport, e.T.Format(time.DateOnly)}})
-	})
+        c.HTML(http.StatusOK, "entry.html", gin.H{"entry": entryFormat{e.Id, e.Sys, e.Dys, e.Puls, e.Sport, e.T.Format(time.DateOnly)}})
+    })
 
-	r.PUT("/entry/:Id", func(c *gin.Context) {
-		param := c.Param("Id")
-		id, err := strconv.ParseInt(param, 10, 64)
-		s, _ := strconv.ParseInt(c.Request.FormValue("sys"), 10, 64)
-		d, _ := strconv.ParseInt(c.Request.FormValue("dys"), 10, 64)
-		p, _ := strconv.ParseInt(c.Request.FormValue("puls"), 10, 64)
-		sp, _ := strconv.ParseBool(c.Request.FormValue("sport"))
+    r.PUT("/entry/:Id", func(c *gin.Context) {
+        param := c.Param("Id")
+        id, err := strconv.ParseInt(param, 10, 64)
+        s, _ := strconv.ParseInt(c.Request.FormValue("sys"), 10, 64)
+        d, _ := strconv.ParseInt(c.Request.FormValue("dys"), 10, 64)
+        p, _ := strconv.ParseInt(c.Request.FormValue("puls"), 10, 64)
+        sp, _ := strconv.ParseBool(c.Request.FormValue("sport"))
 
-		if err != nil {
-			log.Fatal(err)
-		}
+        if err != nil {
+            log.Fatal(err)
+        }
 
-		var e entry
-		e, err = readEntry(db, int(id))
+        var e entry
+        e, err = readEntry(db, int(id))
 
-		e.Sys = s
-		e.Dys = d
-		e.Sport = sp
-		e.Puls = p
+        e.Sys = s
+        e.Dys = d
+        e.Sport = sp
+        e.Puls = p
 
-		updateEntry(e, db)
+        updateEntry(e, db)
 
-		c.HTML(http.StatusOK, "entry.html", gin.H{"entry": entryFormat{e.Id, e.Sys, e.Dys, e.Puls, e.Sport, e.T.Format(time.DateOnly)}})
-	})
+        c.HTML(http.StatusOK, "entry.html", gin.H{"entry": entryFormat{e.Id, e.Sys, e.Dys, e.Puls, e.Sport, e.T.Format(time.DateOnly)}})
+    })
 
-	r.GET("/editrow/:Id", func(c *gin.Context) {
-		param := c.Param("Id")
-		id, err := strconv.ParseInt(param, 10, 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		var e entry
-		e, err = readEntry(db, int(id))
+    r.GET("/editrow/:Id", func(c *gin.Context) {
+        param := c.Param("Id")
+        id, err := strconv.ParseInt(param, 10, 64)
+        if err != nil {
+            log.Fatal(err)
+        }
+        var e entry
+        e, err = readEntry(db, int(id))
 
-		output := entryFormat{e.Id, e.Sys, e.Dys, e.Puls, e.Sport, e.T.Format(time.DateOnly)}
+        output := entryFormat{e.Id, e.Sys, e.Dys, e.Puls, e.Sport, e.T.Format(time.DateOnly)}
 
-		c.HTML(http.StatusOK, "editrow.html", gin.H{"entry": output})
-	})
+        c.HTML(http.StatusOK, "editrow.html", gin.H{"entry": output})
+    })
 
-	r.GET("/addnew", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "addnew.html", gin.H{})
-	})
+    r.GET("/addnew", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "addnew.html", gin.H{})
+    })
 
-	r.POST("/add", func(c *gin.Context) {
-		s, _ := strconv.ParseInt(c.PostForm("sys"), 10, 64)
-		d, _ := strconv.ParseInt(c.PostForm("dys"), 10, 64)
-		p, _ := strconv.ParseInt(c.PostForm("puls"), 10, 64)
+    r.POST("/add", func(c *gin.Context) {
+        s, _ := strconv.ParseInt(c.PostForm("sys"), 10, 64)
+        d, _ := strconv.ParseInt(c.PostForm("dys"), 10, 64)
+        p, _ := strconv.ParseInt(c.PostForm("puls"), 10, 64)
 
-		sp := false
-		o := c.PostForm("sport")
-		if o == "on" {
-			sp = true
-		}
+        sp := false
+        o := c.PostForm("sport")
+        if o == "on" {
+            sp = true
+        }
 
-		e := entry{
-			0,
-			s,
-			d,
-			p,
-			sp,
-			time.Now(),
-		}
+        e := entry{
+            0,
+            s,
+            d,
+            p,
+            sp,
+            time.Now(),
+        }
 
-		writeEntry(e, db)
-	})
+        writeEntry(e, db)
+    })
 
-	r.DELETE("/delete/:Id", func(c *gin.Context) {
-		param := c.Param("Id")
-		id, err := strconv.ParseInt(param, 10, 64)
-		if err != nil {
-			log.Fatal(err)
-		}
+    r.DELETE("/delete/:Id", func(c *gin.Context) {
+        param := c.Param("Id")
+        id, err := strconv.ParseInt(param, 10, 64)
+        if err != nil {
+            log.Fatal(err)
+        }
 
-		err = deleteEntry(db, int(id))
+        err = deleteEntry(db, int(id))
 
-		c.HTML(http.StatusOK, "empty.html", gin.H{})
-	})
+        c.AbortWithStatus(http.StatusNoContent)
+    })
 
-	r.StaticFile("/output.css", "./statics/css/output.css")
+    r.StaticFile("/output.css", "./statics/css/output.css")
 
-	fmt.Printf("Starting server at port 8080\n")
-	if err := r.Run(":8888"); err != nil {
-		log.Fatal(err)
+    fmt.Printf("Starting server at port 8080\n")
+    if err := r.Run(":8888"); err != nil {
+        log.Fatal(err)
 
-	}
-	defer db.Close()
+    }
+    defer db.Close()
 }
